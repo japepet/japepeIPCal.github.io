b599048a1c034aec65d662fddfcf8e8464f65ad7
japepet <jppogi849@gmail.com>
2025-08-08 23:20:56 -0800

    Add files via upload

---
+++ main .js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+    }
+  });
+
+  win.loadFile('ipcaln.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
